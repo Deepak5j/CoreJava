bdc628676d4c73c1c916477a73875ac3bd509bae
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2024-12-27 16:20:29 +0530

    simple java program

---
+++ CoreJava/src/basics/simpleProgram.java
@@ -0,0 +1,15 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 27 Dec 2024 @ 4:18:22 pm
+ */
+
+package basics;
+
+public class simpleProgram {
+	public static void main(String[] args) {
+		String name = "Deepak Gautam";
+		System.out.println("My name is " + name);
+	}
+}
+
