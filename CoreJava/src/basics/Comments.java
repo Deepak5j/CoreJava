07bca02bc5c0e927949fb5ab8ffc53c19b3387aa
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2024-12-27 15:33:04 +0530

    comments

---
+++ CoreJava/src/basics/Comments.java
@@ -0,0 +1,17 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 27 Dec 2024 @ 3:29:39 pm
+ */
+
+package basics;
+
+public class Comments {
+	public static void main(String[] args) {
+		System.out.println("// This is a single line comment.");
+		System.out.println("/* This is a multi line comment*/");
+		System.out.println("/** This is documentation comment */");
+	}
+
+}
+
