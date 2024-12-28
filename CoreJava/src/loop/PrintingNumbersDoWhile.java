ed2ff8e3b1b54813587adc511026f190c376ef70
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2024-12-28 09:16:33 +0530

    Printing Numbers Do While

---
+++ CoreJava/src/loop/PrintingNumbersDoWhile.java
@@ -0,0 +1,19 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 28 Dec 2024 @ 9:15:00 am
+ */
+
+package loop;
+
+public class PrintingNumbersDoWhile {
+	public static void main(String[] args) {
+		int i = 1;
+		do {
+			System.out.print(i + " ");
+			i++;
+		} while(i <= 10);
+	}	
+
+}
+
