3de6f13c3c4afc93f7e638c10baddc5110b8c5b7
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2024-12-28 09:56:14 +0530

    Even Numbers between 10 and 100

---
+++ CoreJava/src/loop/EvenNumbers10_100.java
@@ -0,0 +1,19 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 28 Dec 2024 @ 9:54:24 am
+ */
+
+package loop;
+
+public class EvenNumbers10_100 {
+	public static void main(String[] args) {
+		int i = 10;
+		while (i <= 100) {
+			System.out.print(i + " ");
+			i += 2;
+		}
+	}
+
+}
+
