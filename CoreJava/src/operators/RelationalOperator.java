b2ec2c419beb8e8bce703eb771268bfab22910e9
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2024-12-28 09:08:29 +0530

    Relational Operator

---
+++ CoreJava/src/operators/RelationalOperator.java
@@ -0,0 +1,21 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 28 Dec 2024 @ 9:06:32 am
+ */
+
+package operators;
+
+public class RelationalOperator {
+	public static void main(String[] args) {
+		int a = 15;
+		int b = 10;
+		
+		System.out.println(a < b);
+		System.out.println(a > b);
+		System.out.println(a <= b);
+		System.out.println(a >= b);
+	}
+
+}
+
