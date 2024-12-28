1ac674c8a129c3c860ad7f124cf52e0feecd18c9
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2024-12-28 09:12:29 +0530

    print number

---
+++ CoreJava/src/loop/PrintNumber.java
@@ -0,0 +1,19 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 28 Dec 2024 @ 9:10:18 am
+ */
+
+package loop;
+
+public class PrintNumber {
+	public static void main(String[] args) {
+		int i = 1;
+		while (i <= 20) {
+			System.out.print(i + " ");
+			i++;
+		}
+	}
+
+}
+
