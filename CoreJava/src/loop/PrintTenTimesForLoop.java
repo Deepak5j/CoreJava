d1ea4ecfb9d1e6c1ef41a0b8405575a112862f3e
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2024-12-28 09:19:16 +0530

    Print tentimes using FOR Loop

---
+++ CoreJava/src/loop/PrintTenTimesForLoop.java
@@ -0,0 +1,17 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 28 Dec 2024 @ 9:16:46 am
+ */
+
+package loop;
+
+public class PrintTenTimesForLoop {
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + ". Bright IT Career");
+		}
+	}
+
+}
+
