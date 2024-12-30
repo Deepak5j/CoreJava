4da9a96e618a71cb2dd1477e3088f554ca2a4168
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2024-12-31 04:58:42 +0530

    inheritance, creating super class A

---
+++ CoreJava/src/inheritance/A.java
@@ -0,0 +1,22 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 31 Dec 2024 @ 4:54:59 am
+ */
+
+package inheritance;
+
+// super c;ass
+public class A {
+	int n = 11;
+	void methodA1() {
+		System.out.println("This is class A method 1");
+	}
+	void method2() {
+		System.out.println("This is class A method 2");
+	}
+	void method3() {
+		System.out.println("This is override method - class A");
+	}
+}
+
