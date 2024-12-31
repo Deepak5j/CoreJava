37173dbb42388e4ec2e65d4a003126869e43a5d6
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2024-12-31 15:09:32 +0530

    abstract Class

---
+++ CoreJava/src/abstractClass/Animal.java
@@ -0,0 +1,29 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 31 Dec 2024 @ 3:06:35 pm
+ */
+
+package abstractClass;
+
+abstract class Animal {
+	abstract void sound();
+	void eat() {
+		System.out.println("Animal is eating");
+	}
+}
+
+class Lion extends Animal {
+	void sound() {
+		System.out.println("Lion roars");
+	}
+	public static void main(String[] args) {
+		Animal a = new Lion();
+		a.eat();
+		
+		Lion l = new Lion();
+		l.sound();
+		l.eat();
+	}
+}
+
