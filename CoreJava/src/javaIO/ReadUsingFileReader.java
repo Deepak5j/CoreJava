92d59b5b6d891083a7b41e01d9f649355f2ab079
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-03 09:31:06 +0530

    Read Using FileReader only. Smallest code

---
+++ CoreJava/src/javaIO/ReadUsingFileReader.java
@@ -0,0 +1,23 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 3 Jan 2025 @ 9:28:22 am
+ */
+
+package javaIO;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadUsingFileReader {
+	try(FileReader fr = new FileReader("fw.txt")) {
+		int data;
+		while((data = fr.read()) != -1) {
+			System.out.print((char)data);
+		}
+	}catch(IOException e) {
+		e.printStackTrace();
+	}
+
+}
+
