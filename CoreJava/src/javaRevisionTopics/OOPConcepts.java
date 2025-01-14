baf003f412ed0f64a277824006c0745088a57b30
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-15 05:16:28 +0530

    OOPConcepts - Encapsulation

---
+++ CoreJava/src/javaRevisionTopics/OOPConcepts.java
@@ -0,0 +1,37 @@
+/**
+ * @author Deepak Gautam
+ * @version 1.0
+ * @since 15 Jan 2025 @ 5:10:58 am
+ */
+
+package javaRevisionTopics;
+
+public class OOPConcepts {
+	
+
+}
+
+// Encapsulation
+class Employee {
+	private String name;
+	private int id;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+}
+
+
+
